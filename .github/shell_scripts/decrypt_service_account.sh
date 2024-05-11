e3ff523bfe1352ff4e3ee2bfcd21834849bbcc6f
kiiru4reals <smaina991@gmail.com>
2024-05-11 20:50:13 +0300

    Changed terraform directory

--- .github/shell_scripts/decrypt_service_account.sh
+++ .github/shell_scripts/decrypt_service_account.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-file_name=${GITHUB_WORKSPACE}/gcp/key.json.gpg
+file_name=${GITHUB_WORKSPACE}/terraform/key.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SERVICE_ACCOUNT_PASSPHRASE" --output ${GITHUB_WORKSPACE}/gcp/key.json $file_name
+gpg --quiet --batch --yes --decrypt --passphrase="$GCP_SERVICE_ACCOUNT_PASSPHRASE" --output ${GITHUB_WORKSPACE}/terraform/key.json $file_name
